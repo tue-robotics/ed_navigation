f5b5f60bfc2c73f67abeaf819260c68efff85c72
Rein Appeldoorn <reinzor@gmail.com>
2015-02-27 14:52:45 +0100

    Working symbolic nav constraints

--- src/navigation_plugin.cpp
+++ src/navigation_plugin.cpp
@@ -1,6 +1,7 @@
 #include "ed_navigation/navigation_plugin.h"
 
 #include <ed/entity.h>
+#include <tue/config/reader.h>
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -54,109 +55,84 @@ bool NavigationPlugin::srvGetGoalConstraint(const ed_navigation::GetGoalConstrai
 {
     ed::EntityConstPtr e = world_->getEntity(req.entity_id);
 
-//    if (!e)
-//    {
-//        res.error_msg = "No such entity: '" + req.entity_id + "'.";
-//        return true;
-//    }
+    if (!e)
+    {
+        res.error_msg = "No such entity: '" + req.entity_id + "'.";
+        return true;
+    }
 
-//    const tue::config::DataConstPointer& data = e->data();
-//    if (data.empty())
-//    {
-//        res.error_msg = "Entity '" + e->id().str() + "': does not have an 'areas' property.";
-//        return true;
-//    }
+    const tue::config::DataConstPointer& data = e->data();
+    if (data.empty())
+    {
+        res.error_msg = "Entity '" + e->id().str() + "': does not have an 'areas' property.";
+        return true;
+    }
 
-//    tue::config::Reader r(data);
-//    if (!r.readArray("areas"))
-//    {
-//        res.error_msg = "Entity '" + e->id().str() + "': does not have an 'areas' property.";
-//        return true;
-//    }
+    tue::config::Reader r(data);
+    if (!r.readArray("areas"))
+    {
+        res.error_msg = "Entity '" + e->id().str() + "': does not have an 'areas' property.";
+        return true;
+    }
 
-//    while(r.nextArrayItem())
-//    {
-//        std::string name;
-//        if (r.value("name", name) && name == req.area_name)
-//        {
-//            if (!r.readArray("shape", tue::config::REQUIRED))
-//            {
-//                res.error_msg = "Entity '" + e->id().str() + "': area '" + req.area_name + "' does not have 'shape' property.";
-//                return true;
-//            }
+    while(r.nextArrayItem())
+    {
+        std::string name;
+        if (r.value("name", name) && name == req.area_name)
+        {
+            if (!r.readArray("shape", tue::config::REQUIRED))
+            {
+                res.error_msg = "Entity '" + e->id().str() + "': area '" + req.area_name + "' does not have 'shape' property.";
+                return true;
+            }
 
-//            // Construct position constraint from shape
-//            std::string& pc = res.position_constraint;
+            // Construct position constraint from shape
+            std::string& pc = res.position_constraint;
 
-//            while(r.nextArrayItem())
-//            {
-//                if (r.readGroup("box"))
-//                {
-//                    geo::Vector3 min, max;
+            while(r.nextArrayItem())
+            {
+                if (r.readGroup("box"))
+                {
+                    geo::Vector3 min, max;
 
-//                    if (r.readGroup("min"))
-//                    {
-//                        r.value("x", min.x);
-//                        r.value("y", min.y);
-//                        r.value("z", min.z);
-//                        r.endGroup();
-//                    }
+                    if (r.readGroup("min"))
+                    {
+                        r.value("x", min.x);
+                        r.value("y", min.y);
+                        r.value("z", min.z);
+                        r.endGroup();
+                    }
 
-//                    if (r.readGroup("max"))
-//                    {
-//                        r.value("x", max.x);
-//                        r.value("y", max.y);
-//                        r.value("z", max.z);
-//                        r.endGroup();
-//                    }
+                    if (r.readGroup("max"))
+                    {
+                        r.value("x", max.x);
+                        r.value("y", max.y);
+                        r.value("z", max.z);
+                        r.endGroup();
+                    }
 
-//                    std::cout << "Box: " << min << " - " << max << std::endl;
+                    std::stringstream constraint;
+                    constraint << "x > " << min.x << " and "
+                               << "x < " << max.x << " and "
+                               << "y > " << min.y << " and "
+                               << "y < " << max.y;
 
-                    // Add to position constraint here:
+                    res.position_constraint = constraint.str();
+                    res.frame_id = e->id().str();
 
-                    // ....
+                    r.endGroup();
+                }
+            }
 
-//                    By Rokus from navigate_to_observe.py
+            r.endArray();
 
-//                    x = e.pose.position.x
-//                    y = e.pose.position.y
+            return true;
+        }
+    }
 
-//                    ch.append(ch[0])
+    r.endArray();
 
-//                    pci = ""
-
-//                    for i in xrange(len(ch) - 1):
-//                        dx = ch[i+1].x - ch[i].x
-//                        dy = ch[i+1].y - ch[i].y
-
-//                        length = (dx * dx + dy * dy)**.5
-
-//                        xs = ch[i].x + (dy/length)*self.radius
-//                        ys = ch[i].y - (dx/length)*self.radius
-
-//                        if i != 0:
-//                            pci = pci + ' and '
-
-//                        pci = pci + "-(x-%f)*%f+(y-%f)*%f > 0.0 "%(xs, dy, ys, dx)
-
-//                    pc = PositionConstraint(constraint=pci, frame="/map")
-//                    oc = OrientationConstraint(look_at=Point(x, y, 0.0), frame="/map")
-
-//                    pc += "Something";
-
-//                    r.endGroup();
-//                }
-//            }
-
-//            r.endArray();
-
-//            return true;
-//        }
-//    }
-
-//    r.endArray();
-
-//    res.error_msg = "Entity '" + e->id().str() + "': area '" + req.area_name + "' does not exist";
+    res.error_msg = "Entity '" + e->id().str() + "': area '" + req.area_name + "' does not exist";
     return true;
 }
 
